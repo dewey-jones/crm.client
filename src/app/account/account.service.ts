5fb9e583f569adb118673fc1b4399be01cc854f2
Dewey Jones <dwjones@cognitioninc.com>
2019-12-10 07:54:08 -0700

    Added alert service

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -20,30 +20,9 @@ export class AccountService {
     register(registration: IRegistration): any {
       console.log("registration", registration);
       return this._http.post<IRegistration>(this._accountUrl, registration).pipe(
-          tap(data => console.log('All: ' + JSON.stringify(data))),
+          tap(data => console.log('Registered: ' + JSON.stringify(data))),
           catchError(this.handleError),);
     }
-
-    // getCompany(id: number): Observable<ICompany> {
-    //     return this._http.get<ICompany>(this._companyUrl + "/" + id)
-    //         .do(data => console.log('All: ' + JSON.stringify(data)))
-    //         .catch(this.handleError);
-    // }
-
-    // saveCompany(company: ICompany): any {
-    //     const id = company.id;
-    //     console.log("in service", company);
-    //     console.log("URL", this._companyUrl + "/" + id);
-    //     return this._http.put(this._companyUrl + "/" + id, company)
-    //         .do(data => console.log('All: ' + JSON.stringify(data)))
-    //         .catch(this.handleError);
-    // }
-
-    // deleteCompany(id: number): any {
-    //     return this._http.delete(this._companyUrl + "/" + id)
-    //         .do(data => console.log('All: ' + JSON.stringify(data)))
-    //         .catch(this.handleError);
-    // }
 
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // import { AlertService } from '../_services/index';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
 import { AccountService } from '../account.service';
 import { IRegistration } from '../registration';
+import { AlertService } from '../../shared/alerts';
+// import { AlertService, UserService, AuthenticationService } from '@/_services';
 
 @Component({
   selector: 'pm-register',
@@ -10,30 +15,75 @@ import { IRegistration } from '../registration';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
   pageTitle: string = 'Register';
   registration: IRegistration;
-  
-  constructor(
-    private router: Router,
-    private accountService: AccountService
-  ) {}
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService,
+    private accountService: AccountService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
   }
 
-  save() {
-    // this.loading = true;
-    this.accountService.register(this.registration)
-        .subscribe(
-            data => {
-                // set success message and pass true paramater to persist the message after redirecting to the login page
-                // this.alertService.success('Registration successful', true);
-                this.router.navigate(['/login']);
-            },
-            error => {
-                // this.alertService.error(error);
-                // this.loading = false;
-            });
-}
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
+
+  /*     // this.loading = true;
+      this.accountService.register(this.registration)
+        .subscribe(
+          data => {
+            // set success message and pass true paramater to persist the message after redirecting to the login page
+            // this.alertService.success('Registration successful', true);
+            this.router.navigate(['/login']);
+          },
+          error => {
+            // this.alertService.error(error);
+            // this.loading = false;
+          });
+    }
+   */
 }
